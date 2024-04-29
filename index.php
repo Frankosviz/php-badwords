6d2a3834611f3c9caf8888d957f0c3ab00756d9e
Frankosviz <geom.francescopaolo@gmail.com>
2024-04-29 14:43:31 +0200

    PHP Badwords : Start commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/14dd55bd5d.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../bootstrap.css">
+    <title>PHP Badwords</title>
+</head>
+
+<body>
+    <h1>PHP Badwords</h1>
+    <h3>Inserisci le parole</h3>
+    <div class="container text-center">
+        <form class="form" action="" name="form">
+            <input type="text">
+            <button class="btn btn-primary" type="submit">Invia</button>
+        </form>
+    </div>
+</body>
+
+
